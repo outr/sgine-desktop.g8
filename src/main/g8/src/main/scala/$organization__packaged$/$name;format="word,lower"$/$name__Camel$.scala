b7286c3217b4e3a3b3bf71ba8afba0a171e78bdb
Matt Hicks <matt@outr.com>
2015-12-21 19:27:48 -0600

    Bug fixes and added sgine.png.

--- src/main/g8/src/main/scala/$organization__packaged$/$name;format="word,lower"$/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$name;format="word,lower"$/$name__Camel$.scala
@@ -3,7 +3,7 @@ package $organization$.$name;format="word,lower"$
 import org.sgine._
 import org.sgine.widget.Image
 
-class $name;format="Camel"$Screen extends Screen {
+class $name$Screen extends Screen {
   this += new Image("sgine.png") {
     position.center := ui.width / 2.0
     position.middle := ui.height / 2.0
--- src/main/g8/src/main/scala/$organization__packaged$/$name;format="word,lower"$/Main.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$name;format="word,lower"$/Main.scala
@@ -3,7 +3,7 @@ package $organization$.$name;format="word,lower"$
 import org.sgine.lwjgl.DesktopApp
 
 object Main extends DesktopApp {
-  val hello: $name;format="Camel"$Screen = new $name;format="Camel"$Screen
+  val hello: $name$Screen = new $name$Screen
 
   activeScreens.add(hello)
 }
